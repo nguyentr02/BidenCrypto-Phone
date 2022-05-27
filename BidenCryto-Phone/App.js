15a8626ffcfa3adffd0e639c18d46b660d05b9d7
DNDAHieuBeo <90563964+DNDAHieuBeo@users.noreply.github.com>
2022-05-27 08:30:09 +0700

    no message

--- BidenCryto-Phone/App.js
+++ BidenCryto-Phone/App.js
@@ -1,8 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
-import MainContainer from "./navigation/MainContainer";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function App() {
-  return (<MainContainer />);
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>lz minh hung ngu lam
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
