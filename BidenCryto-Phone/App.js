809c375dfdc7e19bb2c72f5f30e8f9147532013e
DNDAHieuBeo <90563964+DNDAHieuBeo@users.noreply.github.com>
2022-05-27 08:20:10 +0700

    build-navigation-bar

--- BidenCryto-Phone/App.js
+++ BidenCryto-Phone/App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import MainContainer from "./navigation/MainContainer";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>lz minh hung ngu lam
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  return (<MainContainer />);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
--- BidenCryto-Phone/navigation/screens/HomeScreen.js
+++ BidenCryto-Phone/navigation/screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export default function HomeScreen ({navigation}) {
     return(
-        <View>
+        <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
             <Text
             onPress={() => alert('This is the "Home" screen.')}
             style={{fontSize:26,fontWeight:'bold'}}
